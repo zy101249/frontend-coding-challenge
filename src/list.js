b4b30804a146ce93f390e93d69cffff3e9d70325
Zack Yu <zack.yu2@gmail.com>
2017-02-23 14:54:33 -0500

    RetrieveEvents function

--- src/list.js
+++ src/list.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
 class List extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.retrieveEvents = this.retrieveEvents.bind(this);
   }
 
   componentDidMount() {
-    const options = {
-      method: "GET",
-      body: {
-        "username": "candidate.5545@eventable.com",
-        "password": "R8VMaFVK"
-      },
-      headers: {
-      "Content-Type": "application/json"
-      }
-    }
-    fetch('/v1/events', options).then(
-      (response) => { console.log(response) },
-      (error) => { console.log(error) }
-    )
+    this.retrieveEvents();
   }
+
+  retrieveEvents() {
+    const myHeaders = {
+      "Authorization": "Token 7761e7e3b25a1d6d315901fcd7180d971f77ea2e",
+      "Content-Type": "application/json"
+    }
+
+    const options = {
+      method: 'GET',
+      headers:  myHeaders,
+    }
+
+    const url = "https://api.eventable.com/v1/events/?format=json"
+
+    fetch(url, options).then(
+    (response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState(json.results);
+      console.log(this.state)
+    }).catch((ex) => {
+      console.log('parsing failed', ex)
+    })
+  }
+
 
   render() {
     return (
